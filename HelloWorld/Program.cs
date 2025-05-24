b6d0065f483dee2deae79f3820ade8540e8700c2
Long Dong <longdong32120@gmail.com>
2025-05-24 20:34:50 +0700

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I am Longie");
+            Console.WriteLine("Longie is 20 yo");
+            Console.ReadLine();
+        }
+    }
+}
