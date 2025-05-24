813d7f14568b4088bb68c08bf0f1d5ef33ef1061
Long Dong <longdong32120@gmail.com>
2025-05-24 20:19:08 +0700

    Add files via upload

---
+++ CallByReference/Program.cs
@@ -0,0 +1,16 @@
+﻿void swap(ref int a,ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    Console.WriteLine("a trong hàm =" + a);
+    Console.WriteLine("b trong hàm =" + b);
+}
+int a = 5;
+int b = 10;
+Console.WriteLine("a trước khi vào hàm =" + a);
+Console.WriteLine("b trước khi vào hàm =" + b);
+swap(ref a,ref b);
+Console.WriteLine("a sau khi vào hàm =" + a);
+Console.WriteLine("b sau khi vào hàm =" + b);
+Console.ReadLine();
